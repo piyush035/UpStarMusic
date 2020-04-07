26bee6a3b69e526089fe6bf6ffc2c49f1ef23fcd
Piyush Gupta <piyush035@gmail.com>
2020-04-07 23:12:39 +0530

    Update

--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -13,7 +13,7 @@ module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
     .sort({ [sortProperty]: 1 })
     .skip(offset)
     .limit(limit);
-  return Promise.all([query, Artist.count()]).then((results) => {
+  return Promise.all([query, Artist.find(buildQuery(criteria)).count()]).then((results) => {
     return { all: results[0], count: results[1], offset: offset, limit: limit };
   });
 };
